fc33235a6f7a4ad26eab50419ac7903512182a37
ariannaoc <ariannaolivares@gmail.com>
2024-08-09 18:30:57 -0400

    carousel experience

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+function Carousel({ images }) {
+  const [index, setIndex] = useState(0);
+
+  const handlePrev = () => {
+    setIndex((index - 1 + images.length) % images.length);
+  };
+
+  const handleNext = () => {
+    setIndex((index + 1) % images.length);
+  };
+
+  return (
+    <>
+      <div className="m-auto flex">
+        <button
+          className=" opacity-75 rounded-full h-fit my-auto"
+          onClick={handlePrev}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 dark:stroke-bunker-200 h-10 w-10 "
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
+        </button>
+
+        <img src={images[index]} alt="" className="h-72 rounded-xl shadow-xl" />
+
+        <button
+          className=" opacity-75 rounded-full h-fit my-auto"
+          onClick={handleNext}
+        >
+          {/* <img src="/right.svg" alt=">" className="w-10 dark:stroke-bunker-400" /> */}
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 dark:stroke-bunker-200 w-10 h-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Carousel;
